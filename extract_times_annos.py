acbfa0386ea7154ebf62f9c64f257ec29af300ce
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-08-09 12:28:41 -0400

    moved to xmlutils

--- extract_times_annos.py
+++ extract_times_annos.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as bs
+from xml_utils import populate_times, populate_annos
 
 def get_bs_content(file_name):
 
@@ -7,47 +8,6 @@ def get_bs_content(file_name):
     content = "".join(content)
     bs_content = bs(content, features="xml")
     return bs_content
-
-
-def populate_times(root):
-
-    found_time = False
-    time_dict = dict()
-    children = root.children
-
-    for child in children:
-
-        # stop looking after we found time specs
-        if found_time:
-            break
-
-        if child == "\n":
-            pass
-        else:
-            try:
-                l_content = len(child.contents)
-                for i in range(l_content):
-                    if child.contents[i] == "\n":
-                        pass
-                    else:
-                        try:
-                            k = child.contents[i]["TIME_SLOT_ID"]
-                            v = child.contents[i]["TIME_VALUE"]
-                            time_dict[k] = v
-                            if not found_time:
-                                found_time = True
-                        except KeyError:
-                            pass
-
-            except TypeError:
-                pass
-
-    print(f"Number of time segments found = {len(time_dict.keys())}")
-    return time_dict
-
-
-def populate_annos(root):
-    pass
 
 
 if __name__ == "__main__":
