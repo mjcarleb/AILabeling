42a5d593ec6dd91f9e6b74c7b0ea12b47736b93a
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-08-09 13:08:57 -0400

    working

--- extract_times_annos.py
+++ extract_times_annos.py
@@ -9,6 +9,22 @@ def get_bs_content(file_name):
     bs_content = bs(content, features="xml")
     return bs_content
 
+def recast_annos_dict(annos_dict, time_dict):
+
+    new_dict = dict()
+
+    for start_time_ref in annos_dict.keys():
+
+        dict_value = annos_dict[start_time_ref]
+
+        end_time_ref = dict_value[1]  # see xml_utils.py
+
+        start_time_ms = time_dict[start_time_ref]
+        end_time_ms = time_dict[end_time_ref]
+
+        new_dict[start_time_ms] = [dict_value[0], end_time_ms, dict_value[2]]
+
+    return new_dict
 
 if __name__ == "__main__":
 
@@ -17,4 +33,6 @@ if __name__ == "__main__":
     root = next(bs_content.children)
 
     time_dict = populate_times(root)
-    anno_dict = populate_annos(root)
+    annos_dict = populate_annos(root)
+    annos_dict = recast_annos_dict(annos_dict, time_dict)
+    a=3
