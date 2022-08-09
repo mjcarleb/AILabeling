279376638c2c1b261ddf3db3ae9b4982a7d9d762
Mark Carlebach <markcarlebach@marks-mbp.myfiosgateway.com>
2022-08-09 13:48:17 -0400

    now working in frames not ms

--- extract_times_annos.py
+++ extract_times_annos.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup as bs
-from xml_utils import populate_times, populate_annos
+from xml_utils import populate_times, populate_annos, recast_annos_absT, get_mspf, recast_annos_frames
 
 def get_bs_content(file_name):
 
@@ -9,30 +9,17 @@ def get_bs_content(file_name):
     bs_content = bs(content, features="xml")
     return bs_content
 
-def recast_annos_dict(annos_dict, time_dict):
-
-    new_dict = dict()
-
-    for start_time_ref in annos_dict.keys():
-
-        dict_value = annos_dict[start_time_ref]
-
-        end_time_ref = dict_value[1]  # see xml_utils.py
-
-        start_time_ms = time_dict[start_time_ref]
-        end_time_ms = time_dict[end_time_ref]
-
-        new_dict[start_time_ms] = [dict_value[0], end_time_ms, dict_value[2]]
-
-    return new_dict
-
 if __name__ == "__main__":
 
-    elan_file_name = "data/Cactus 4_8_22_Satya_V3.eaf"
-    bs_content = get_bs_content(elan_file_name)
+    anno_filename = "data/Cactus 4_8_22_Satya_V3.eaf"
+    mp4_filename = "data/Cactus 4_8_22_V3.mp4"
+
+    bs_content = get_bs_content(anno_filename)
     root = next(bs_content.children)
 
     time_dict = populate_times(root)
     annos_dict = populate_annos(root)
-    annos_dict = recast_annos_dict(annos_dict, time_dict)
+    annos_dict = recast_annos_absT(annos_dict, time_dict)
+    mspf = get_mspf(mp4_filename)
+    annos_dict = recast_annos_frames(annos_dict, mspf)
     a=3
--- xml_utils.py
+++ xml_utils.py
@@ -85,3 +85,57 @@ def populate_annos(root):
 
     print(f"Number of annos found = {len(annos_dict.keys())}")
     return annos_dict
+
+
+def recast_annos_absT(annos_dict, time_dict):
+
+    new_dict = dict()
+
+    for start_time_ref in annos_dict.keys():
+
+        dict_value = annos_dict[start_time_ref]
+
+        end_time_ref = dict_value[1]  # see xml_utils.py
+
+        start_time_ms = time_dict[start_time_ref]
+        end_time_ms = time_dict[end_time_ref]
+
+        new_dict[start_time_ms] = [dict_value[0], end_time_ms, dict_value[2]]
+
+    return new_dict
+
+def recast_annos_frames(annos_dict, mspf):
+
+    new_dict = dict()
+
+    for start_ms in annos_dict.keys():
+
+        dict_values = annos_dict[start_ms]
+        end_ms = dict_values[1]
+
+        # + or - 1, floor, round up????
+        start_f = int(int(start_ms) / mspf)
+        end_f = int(int(end_ms) / mspf)
+
+        new_dict[start_f] = [dict_values[0], end_f, dict_values[2]]
+
+    return new_dict
+
+
+def get_mspf(mp4_file):
+    import cv2
+
+    video = cv2.VideoCapture(mp4_file)
+
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+    if int(major_ver) < 3:
+        fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+    else:
+        fps = video.get(cv2.CAP_PROP_FPS)
+
+    spf = 1 / fps
+    mspf = spf * 1000
+    print(f"ms per frame = {mspf}")
+    return mspf
+
+
